16eb9c12082428064cbc17acaf64d8159d322d4a
Brandon Morse <brandon@morsecodemedia.com>
2022-02-03 16:37:20 -0500

    creates color card component

---
+++ components/global/color-card.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="color-cards-container">
+    <div v-for="(c, i) in colors" :key="i" class="color-card">
+      <div :style="{ backgroundColor: c.hex }" class="color-block" />
+      <div class="color-details">
+        <span class="color-name">{{ c.name }}</span>
+        <span class="color-hex">Hex: {{ c.hex }}</span>
+        <span class="color-rgb">RGB: {{ c.rgb }}</span>
+        <span class="color-hsl">HSL: {{ c.hsl }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ColorCard',
+  props: {
+    colors: {
+      type: Object,
+      required: true,
+      twoWay: true,
+      default: () => {}
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.color-cards-container {
+  display: flex;
+  justify-content: space-evenly;
+  .color-card {
+    padding: 10px;
+    border-radius: 15px;
+    border: 1px solid black;
+    display: inline-flex;
+    flex-direction: column;
+    justify-content: center;
+    align-content: center;
+
+    .color-block {
+      height: 200px;
+      width: 200px;
+      border: 1px solid black;
+      margin-bottom: 10px;
+    }
+
+    .color-details {
+      display: flex;
+      flex-direction: column;
+      text-align: left;
+
+      span {
+        margin-bottom: 5px;
+      }
+    }
+  }
+}
+</style>
