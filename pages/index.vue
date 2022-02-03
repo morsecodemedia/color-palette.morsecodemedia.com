a1a4d5210cc2c5d8a827c0cd67d1ed8cbc536c85
Brandon Morse <brandon@morsecodemedia.com>
2022-02-03 10:57:05 -0500

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1>
+    Hello World!
+  </h1>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  head () {
+    return {
+      title: '',
+      meta: [
+        { hid: 'ogtitle', property: 'og:title', content: '' },
+        { hid: 'twtitle', name: 'twitter:title', content: '' },
+        { hid: 'googlename', itemprop: 'name', content: '' },
+        { hid: 'description', name: 'description', content: '' },
+        { hid: 'ogdescription', property: 'og:description', content: '' },
+        { hid: 'twdescription', name: 'twitter:description', content: '' },
+        { hid: 'googledescription', itemprop: 'description', content: '' },
+        { hid: 'ogurl', property: 'og:url', content: 'https://www.domain.com' + this.$route.path },
+        { hid: 'twsite', name: 'twitter:site', content: 'https://www.domain.com' + this.$route.path }
+      ],
+      link: [
+        { hid: 'canonical', rel: 'canonical', href: 'https://www.domain.com' + this.$route.path }
+      ]
+    }
+  }
+}
+</script>
